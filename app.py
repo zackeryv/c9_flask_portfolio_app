014ae242f7092f2cd2b67efc9ef7353b8c519077
zackeryv <43520097+zackeryv@users.noreply.github.com>
2018-09-24 06:26:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def add_numbers_post():
 	  	return render_template('add_numbers.html')
 	  elif request.method == 'POST':
   	      print(request.form['text'].split())
-  	      total = 0
+  	      total = 1
   	      try:
   	      	for str_num in request.form['text'].split():
   	      		total *= int(str_num)
