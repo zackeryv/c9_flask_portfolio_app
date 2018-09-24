a914fa3c2c529173121c46562bf2b93d400794a6
zackeryv <43520097+zackeryv@users.noreply.github.com>
2018-09-24 06:12:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def home_page():
 
 @app.route('/<name>')
 def profile(name):
-	new_name = name + " likes to eat mangos"
+	new_name = str(name) + " likes to eat mangos"
 	return render_template('index.html', name=new_name)
 
 
